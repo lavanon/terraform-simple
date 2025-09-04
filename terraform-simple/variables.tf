fb25ae4a54af14c7641fa424e6b3b3115b845b7f
codellah <40672446+codellah@users.noreply.github.com>
2025-09-04 12:51:39 +0100

    Update to the regex to fix.

--- terraform-simple/variables.tf
+++ terraform-simple/variables.tf
@@ -23,7 +23,7 @@ variable "bucket_name" {
   type        = string
 
   validation {
-    condition     = length(regexall("^[a-z0-9][a-z0-9.-]{1,61}[a-z0-9]$", var.bucket_name)) > 0 && length(regexall("^(\\d{1,3}\\.){3}\\d{1,3}$", var.bucket_name)) == 0 && !contains(var.bucket_name, "..") && !contains(var.bucket_name, ".-") && !contains(var.bucket_name, "-.")
+    condition     = length(regexall("^[a-z0-9][a-z0-9.-]{1,61}[a-z0-9]$", var.bucket_name)) > 0 && length(regexall("^(\\d{1,3}\\.){3}\\d{1,3}$", var.bucket_name)) == 0 && length(regexall("\\.\\.", var.bucket_name)) == 0 && length(regexall("\\.-", var.bucket_name)) == 0 && length(regexall("-\\.", var.bucket_name)) == 0
     error_message = "bucket_name must be 3-63 chars, lowercase letters, numbers, dots or hyphens; not look like an IP; and not contain '..', '.-' or '-.'."
   }
 }
